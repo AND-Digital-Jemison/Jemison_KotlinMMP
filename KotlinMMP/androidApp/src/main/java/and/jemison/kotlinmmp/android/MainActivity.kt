0b255d7ed5981e0d7f62cc96e01fc9f046052b7c
TomC13K <tomas.ciernik@and.digital>
2021-09-10 14:19:30 +0100

    set up local env variable for APP_CENTER_KEY

--- KotlinMMP/androidApp/src/main/java/and/jemison/kotlinmmp/android/MainActivity.kt
+++ KotlinMMP/androidApp/src/main/java/and/jemison/kotlinmmp/android/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import and.jemison.kotlinmmp.Greeting
 import android.widget.TextView
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 fun greet(): String {
     return Greeting().greeting()
@@ -15,9 +15,9 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appCenterKey = BuildConfig.APP_CENTER_KEY
 
-
-        AppCenter.start(application, BuildConfig.APP_CENTER_KEY,
+        AppCenter.start(application, appCenterKey,
          Analytics::class.java, Crashes::class.java)
 
         setContentView(R.layout.activity_main)
