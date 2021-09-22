811de306751ecea9f5e805b46d6c012a6d6fa9b0
Marilia Elia <you@example.com>
2021-09-22 17:18:30 +0100

    Revert "add apps center sdk for iOS app"
    This reverts commit eab506081a38d85b47d038d2f8619e48a09f65df.

--- KotlinMMP/iosApp/iosApp/iOSApp.swift
+++ KotlinMMP/iosApp/iosApp/iOSApp.swift
@@ -1,13 +1,7 @@
 import SwiftUI
-import AppCenter
-import AppCenterAnalytics
-import AppCenterCrashes
 
 @main
 struct iOSApp: App {
-    init() {
-        AppCenter.start(withAppSecret: "388e8513-eaaf-4c7d-8bca-dfcddcc20128", services: [Analytics.self, Crashes.self])
-    }
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
