a4fe32ab0502bafbfb841487efa8daeb65c45b5c
emilybaileyANDdigital <emily.bailey@and.digital>
2021-10-05 16:08:46 +0100

    swipe testing

--- KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/MainActivityTest.kt
+++ KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/MainActivityTest.kt
@@ -1,14 +1,17 @@
-package and.jemison.kotlinmmp//package and.jemison.kotlinmmp
+package and.jemison.kotlinmmp.android//package and.jemison.kotlinmmp
+
 
 import and.jemison.kotlinmmp.android.MainActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.junit.Assert
+import junit.runner.Version.id
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,7 +24,11 @@ class MainActivityTest {
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun listGoesOverTheFold() {
-        onView(withText("Hello World!")).check(matches(isDisplayed()))
+    fun showMoodOptions() {
+        onView(withText("Doing good")).check(matches(isCompletelyDisplayed()))
+        onView(withText("Doing not so good")).check(doesNotExist())
+        onView(withId(R.id.view_pager)).perform(swipeLeft())
+        onView(withText("Doing not so good")).check(matches(isCompletelyDisplayed()))
+        onView(withText("Doing good")).check(doesNotExist());
     }
 }
