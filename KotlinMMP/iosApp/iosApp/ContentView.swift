6e6b62053a5963b1858974dcbe5ce0aad31c2fde
zainabnoori <zainab.noori@and.digital>
2021-10-07 15:07:42 +0100

    Swipe page for ios

--- KotlinMMP/iosApp/iosApp/ContentView.swift
+++ KotlinMMP/iosApp/iosApp/ContentView.swift
@@ -5,8 +5,37 @@ struct ContentView: View {
 	let greet = Greeting().greeting()
 
 	var body: some View {
-		Text(greet)
+        Text("How are you doing today?")
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .padding(.top, 100)
+            .lineSpacing(10)
+       
+        ZStack{
+            TabView {
+                    Text("I'm Great!")
+                        .foregroundColor(.green)
+                        .bold()
+        
+                    Text("I'm not Well")
+                        .foregroundColor(.red)
+                        .bold()
+            
+                 }
+            .padding(.top,-100)
+            .tabViewStyle( PageTabViewStyle())
+         
+        }
+     
+        Button("Submit") {
+            print("Button tapped!")
+        }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+            .cornerRadius(20)
 	}
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -14,3 +43,5 @@ struct ContentView_Previews: PreviewProvider {
 		ContentView()
 	}
 }
+
+
