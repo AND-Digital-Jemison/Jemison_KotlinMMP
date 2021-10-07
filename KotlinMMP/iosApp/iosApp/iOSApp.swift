2e3df3c990c13cdf7db9a7baab20e834a1887ad6
Jon Earl <jon.earl@and.digital>
2021-10-07 14:15:47 +0100

    app center back in

--- KotlinMMP/iosApp/iosApp/iOSApp.swift
+++ KotlinMMP/iosApp/iosApp/iOSApp.swift
@@ -1,12 +1,12 @@
 import SwiftUI
-//import AppCenter
-//import AppCenterAnalytics
-//import AppCenterCrashes
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct iOSApp: App {
     init() {
-//        AppCenter.start(withAppSecret: "388e8513-eaaf-4c7d-8bca-dfcddcc20128", services: [Analytics.self, Crashes.self])
+        AppCenter.start(withAppSecret: "388e8513-eaaf-4c7d-8bca-dfcddcc20128", services: [Analytics.self, Crashes.self])
     }
 	var body: some Scene {
 		WindowGroup {
