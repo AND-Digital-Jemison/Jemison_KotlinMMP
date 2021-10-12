1b8e2df80ba0528406e1ac0100c2ff5ffdded9f2
Marilia Elia <69245400+MariliaE@users.noreply.github.com>
2021-10-12 15:33:02 +0100

    Ios swipe (#19)
    * Swipe page for ios
    
    * Submit button detecting tab view displayed
    
    * added texts
    
    * UI Testing for swipe and launch page info
    
    * removed not used assets file with images
    
    * removed xcuserdatad file
    
    * test swipe on text element
    
    * one test for swipe left and right
    
    Co-authored-by: zainabnoori <zainab.noori@and.digital>
    Co-authored-by: Jack Rimmer <jack.rimmer@and.digital>
    Co-authored-by: Jon Earl <89974553+jon-earl@users.noreply.github.com>
    Co-authored-by: Jon Earl <jon.earl@and.digital>

--- KotlinMMP/iosApp/iosApp/ContentView.swift
+++ KotlinMMP/iosApp/iosApp/ContentView.swift
@@ -2,11 +2,45 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+    @State private var selectedTab = "great"
 
 	var body: some View {
-		Text(greet)
+        Text("How are you doing today?")
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .padding(.top, 100)
+            .lineSpacing(10)
+        Text("(Swipe to change mood)")
+            .multilineTextAlignment(.center)
+       
+        TabView(selection: $selectedTab) {
+                Text("Doing Great!")
+                    .foregroundColor(.green)
+                    .bold()
+                    .tag("great")
+
+                Text("Not so great")
+                    .foregroundColor(.red)
+                    .bold()
+                    .tag("not great")
+
+
+        }
+        .padding(.top,-100)
+        .tabViewStyle( PageTabViewStyle())
+
+        Text("This is anonymous")
+            .multilineTextAlignment(.center)
+
+        Button("Submit") {
+            print("Button tapped! Response selected: ", selectedTab)
+        }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+            .cornerRadius(20)
 	}
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -14,3 +48,5 @@ struct ContentView_Previews: PreviewProvider {
 		ContentView()
 	}
 }
+
+
--- KotlinMMP/iosApp/iosAppUITests/iosAppUITests.swift
+++ KotlinMMP/iosApp/iosAppUITests/iosAppUITests.swift
@@ -4,23 +4,32 @@ class iosAppUITests: XCTestCase {
     var app: XCUIApplication!
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         app = XCUIApplication()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testHelloIos() throws {
-        // UI tests must launch the application that they test.
+    func testLaunchPageInformation() throws {
         app.launch()
-        let helloWorld = app.staticTexts["Hello, iOS 15.0!"]
-        
-        XCTAssert(helloWorld.exists)
+        let question = app.staticTexts["How are you doing today?"]
+        let answer = app.staticTexts["Doing Great!"]
+        let button = app.buttons.element
+        XCTAssert(question.exists)
+        XCTAssert(answer.exists)
+        XCTAssert(button.exists)
     }
+    
+    func testSwipe() throws {
+           app.launch()
+        
+           let greatText = app.staticTexts["Doing Great!"]
+           XCTAssert(greatText.exists)
+        
+           greatText.swipeLeft()
+           let notGreatText = app.staticTexts["Not so great"]
+           XCTAssert(notGreatText.exists)
+        
+           notGreatText.swipeRight()
+           XCTAssert(greatText.exists)
+       }
+
 }
