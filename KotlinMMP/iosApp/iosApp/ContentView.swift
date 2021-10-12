b44814d986c8a39d88db5d832e57b924fc884df9
MariliaE <marilia.elia@and.digital>
2021-10-12 17:52:32 +0100

    Design for iOS UI and PrimaryButton class to make button reusable

--- KotlinMMP/iosApp/iosApp/ContentView.swift
+++ KotlinMMP/iosApp/iosApp/ContentView.swift
@@ -2,43 +2,70 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
+    
     @State private var selectedTab = "great"
 
 	var body: some View {
-        Text("How are you doing today?")
-            .font(.largeTitle)
-            .multilineTextAlignment(.center)
-            .padding(.top, 100)
-            .lineSpacing(10)
-        Text("(Swipe to change mood)")
-            .multilineTextAlignment(.center)
-       
-        TabView(selection: $selectedTab) {
-                Text("Doing Great!")
-                    .foregroundColor(.green)
-                    .bold()
-                    .tag("great")
+        ZStack() {
+            Image("background-purple")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("How are you doing today?")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 100)
+                    .foregroundColor(.white)
+                
+                Text("(Swipe to change mood)")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                
+                TabView(selection: $selectedTab) {
+                    VStack{
+                        Image("rocket")
+                            .resizable()
+                            .scaledToFit()
+                        Text("Doing Great")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }.tag("great")
+                    
+                    VStack{
+                        Image("rocket")
+                            .resizable()
+                            .scaledToFit()
+                            .rotationEffect(.degrees(180))
+                        Text("Not so great")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            
+                    }.tag("not great")
+                }
+                .padding(.top,-100)
+                .tabViewStyle( PageTabViewStyle())
 
-                Text("Not so great")
-                    .foregroundColor(.red)
-                    .bold()
-                    .tag("not great")
-
-
+                Button("SUBMIT") {
+                    print("Button tapped! Response selected: ", selectedTab)
+                }.buttonStyle(PrimaryButton())
+//                }.padding()
+//                 .frame(minWidth: 0, maxWidth: .infinity)
+//                 .background(Color.gray.opacity(0.3))
+//                 .border(Color.white)
+//                 .cornerRadius(10)
+//                 .foregroundColor(.white)
+                 
+                Text("This is anonymous")
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    
+            
+                   
+            } .padding([.bottom, .leading, .trailing], 20)
         }
-        .padding(.top,-100)
-        .tabViewStyle( PageTabViewStyle())
 
-        Text("This is anonymous")
-            .multilineTextAlignment(.center)
-
-        Button("Submit") {
-            print("Button tapped! Response selected: ", selectedTab)
-        }
-            .padding()
-            .background(Color.black)
-            .foregroundColor(.white)
-            .cornerRadius(20)
 	}
         
 }
