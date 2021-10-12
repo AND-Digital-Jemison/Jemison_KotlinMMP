3efd55f43b2f4dfe29741268c82091d9ee053fbe
Jack Rimmer <jack.rimmer@and.digital>
2021-10-12 11:44:55 +0100

    Try separating swipe into separate tests

--- KotlinMMP/iosApp/iosAppUITests/iosAppUITests.swift
+++ KotlinMMP/iosApp/iosAppUITests/iosAppUITests.swift
@@ -18,21 +18,28 @@ class iosAppUITests: XCTestCase {
         XCTAssert(button.exists)
     }
     
-    func testSwipe() throws {
-        app.launch()
-        
-        let doingGreat = app.staticTexts["Doing Great!"]
-        XCTAssert(doingGreat.exists)
-        
-        app.swipeLeft()
-        
-        let notSoGreat = app.staticTexts["Not so great"]
-        XCTAssert(notSoGreat.exists)
-        XCTAssert(!doingGreat.exists)
-        
-        app.swipeRight()
-        
-        XCTAssert(doingGreat.exists)
-        XCTAssert(!notSoGreat.exists)
-    }
+    func testSwipeRightToLeft() throws {
+           app.launch()
+           app.swipeLeft()
+           
+           let question = app.staticTexts["How are you doing today?"]
+           let answer = app.staticTexts["Not so great"]
+           let button = app.buttons.element
+           XCTAssert(question.exists)
+           XCTAssert(answer.exists)
+           XCTAssert(button.exists)
+       }
+       
+       func testSwipeLeftToRight() throws {
+           app.launch()
+           app.swipeLeft()
+           app.swipeRight()
+           
+           let question = app.staticTexts["How are you doing today?"]
+           let answer = app.staticTexts["Doing Great!"]
+           let button = app.buttons.element
+           XCTAssert(question.exists)
+           XCTAssert(answer.exists)
+           XCTAssert(button.exists)
+       }
 }
