9c6e5afe2113a1acffbbcfd5a042d94301118510
Jack Rimmer <jack.rimmer@and.digital>
2021-10-13 14:39:45 +0100

    Configured gradle to use Jetpack Compose

--- KotlinMMP/androidApp/build.gradle.kts
+++ KotlinMMP/androidApp/build.gradle.kts
@@ -63,12 +63,36 @@ android {
 
     buildFeatures {
         dataBinding = true
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.3"
     }
 
     dependencies {
         androidTestImplementation("androidx.test:runner:1.4.0")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
         implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+        // Jetpack Compose dependencies
+        implementation("androidx.activity:activity-compose:1.3.1")
+        implementation("androidx.compose.material:material:1.0.3")
+        implementation("androidx.compose.animation:animation:1.0.3")
+        implementation("androidx.compose.ui:ui-tooling:1.0.3")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-rc01")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.3")
+
+        implementation("com.google.android.material:compose-theme-adapter:1.0.3")
+        implementation("com.google.accompanist:accompanist-appcompat-theme:0.16.0")
     }
 
 }
