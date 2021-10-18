d81d4f4a585d669b4161a92dccad6dd708c03ca1
Jack Rimmer <jack.rimmer@and.digital>
2021-10-18 16:27:19 +0100

    Added unit testing

--- KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
+++ KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
@@ -1,18 +1,9 @@
 package and.jemison.kotlinmmp.androidapp
 
-import and.jemison.kotlinmmp.androidapp.testutils.ViewPager2IdlingResource
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.swipeLeft
-import androidx.test.espresso.action.ViewActions.swipeRight
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -22,52 +13,32 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    private lateinit var viewPager2IdlingResource: ViewPager2IdlingResource
-
-    @Before
-    fun setUp() {
-        activityRule.scenario.onActivity {
-            viewPager2IdlingResource =
-                ViewPager2IdlingResource(
-                    it.findViewById(R.id.mood_options_view_pager),
-                    "viewPagerIdlingResource"
-                )
-            IdlingRegistry.getInstance().register(viewPager2IdlingResource)
-        }
-    }
-
-    @After
-    fun tearDown() {
-        IdlingRegistry.getInstance().unregister(viewPager2IdlingResource)
+    @Test
+    fun defaultMoodRenders() {
+        composeTestRule.onNodeWithText(GOOD_MOOD_TEXT).assertIsDisplayed()
+        composeTestRule.onNodeWithText(BAD_MOOD_TEXT).assertIsNotDisplayed()
     }
 
     @Test
-    fun showMoodOptions() {
-        checkGoodMoodDisplayed()
+    fun swipeBetweenMoods() {
+        val goodMood = composeTestRule.onNodeWithText(GOOD_MOOD_TEXT)
+        val badMood = composeTestRule.onNodeWithText(BAD_MOOD_TEXT)
 
-        onView(withId(R.id.mood_options_view_pager)).perform(swipeLeft())
+        goodMood.performGesture { swipeLeft() }
 
-        checkBadMoodDisplayed()
+        badMood.assertIsDisplayed()
+        goodMood.assertIsNotDisplayed()
 
-        onView(withId(R.id.mood_options_view_pager)).perform(swipeRight())
+        badMood.performGesture { swipeRight() }
 
-        checkGoodMoodDisplayed()
-    }
-
-    private fun checkGoodMoodDisplayed() {
-        onView(withText(GOOD_MOOD_TEXT)).check(matches(isCompletelyDisplayed()))
-        onView(withText(BAD_MOOD_TEXT)).check(doesNotExist())
-    }
-
-    private fun checkBadMoodDisplayed() {
-        onView(withText(BAD_MOOD_TEXT)).check(matches(isCompletelyDisplayed()))
-        onView(withText(GOOD_MOOD_TEXT)).check(doesNotExist())
+        goodMood.assertIsDisplayed()
+        badMood.assertIsNotDisplayed()
     }
 
     companion object {
         private const val GOOD_MOOD_TEXT = "Doing Great!"
-        private const val BAD_MOOD_TEXT = "Not So Great!"
+        private const val BAD_MOOD_TEXT = "Not So Great"
     }
 }
