1187a2138f080b48f3445fc266b7e11b63790eeb
Jon Earl <jon.earl@and.digital>
2021-10-19 19:24:51 +0100

    pod version build script

--- KotlinMMP/iosApp/ios-build.sh
+++ KotlinMMP/iosApp/ios-build.sh
@@ -1,3 +1,4 @@
+pod --version
 pod install
 gradle -v
 xcodebuild -workspace iosApp.xcworkspace -scheme iosApp -destination 'platform=iOS Simulator,OS=15.0,name=iPhone 13' clean build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
