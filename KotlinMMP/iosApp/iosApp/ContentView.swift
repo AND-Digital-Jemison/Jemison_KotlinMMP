347cba030bce8db3e27a65116c78ea694aa2cac5
Jon Earl <jon.earl@and.digital>
2021-10-20 17:42:58 +0100

    resizeable background

--- KotlinMMP/iosApp/iosApp/ContentView.swift
+++ KotlinMMP/iosApp/iosApp/ContentView.swift
@@ -7,7 +7,7 @@ struct ContentView: View {
 	var body: some View {
         ZStack() {
             Image("background-purple")
-               // .resizable()
+                    .resizable()
 //                .scaledToFill()
                 //.aspectRatio(contentMode: .fit)
 //                .edgesIgnoringSafeArea(.all)
