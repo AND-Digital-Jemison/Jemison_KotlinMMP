6defefd4a63bb6c952dc1233b384f9c77b0a4075
Jack Rimmer <jack.rimmer@and.digital>
2021-10-21 15:38:38 +0100

    Updated gradle file

--- KotlinMMP/androidApp/build.gradle.kts
+++ KotlinMMP/androidApp/build.gradle.kts
@@ -3,8 +3,6 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("kotlin-android")
-    kotlin("kapt")
 }
 
 dependencies {
@@ -14,8 +12,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.test.ext:junit-ktx:1.1.3")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.test.espresso:espresso-idling-resource:3.4.0")
-    implementation("androidx.test.espresso:espresso-contrib:3.4.0")
 
     val appCenterSdkVersion = "4.3.1"
     implementation("com.microsoft.appcenter:appcenter-analytics:${appCenterSdkVersion}")
@@ -62,7 +58,6 @@ android {
     }
 
     buildFeatures {
-        dataBinding = true
         compose = true
     }
 
@@ -84,18 +79,16 @@ android {
 
     dependencies {
         androidTestImplementation("androidx.test:runner:1.4.0")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-        implementation("androidx.viewpager2:viewpager2:1.0.0")
 
         implementation("androidx.activity:activity-compose:1.3.1")
-        implementation("androidx.compose.material:material:1.0.3")
-        implementation("androidx.compose.animation:animation:1.0.3")
-        implementation("androidx.compose.ui:ui-tooling:1.0.3")
+        implementation("androidx.compose.material:material:1.0.4")
+        implementation("androidx.compose.animation:animation:1.0.4")
+        implementation("androidx.compose.ui:ui-tooling:1.0.4")
         implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-rc01")
-        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.3")
-        debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.3")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.4")
+        debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.4")
 
-        implementation("com.google.android.material:compose-theme-adapter:1.0.3")
+        implementation("com.google.android.material:compose-theme-adapter:1.0.4")
         implementation("com.google.accompanist:accompanist-appcompat-theme:0.16.0")
         implementation("com.google.accompanist:accompanist-pager:0.19.0")
     }
