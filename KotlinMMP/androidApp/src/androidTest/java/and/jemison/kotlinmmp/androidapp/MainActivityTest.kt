18d7a646bd7a0f892d880d6ed7de7ae0810806db
Jack Rimmer <jack.rimmer@and.digital>
2021-10-22 11:50:22 +0100

    Uncomment pager display test

--- KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
+++ KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
@@ -19,9 +19,9 @@ class MainActivityTest {
     fun pageRenders() {
         composeTestRule.onNodeWithText(HOW_ARE_YOU_TEXT).assertIsDisplayed()
         composeTestRule.onNodeWithText(SWIPE_HELP_TEXT).assertIsDisplayed()
-//
-//        val defaultMoodText = composeTestRule.onNodeWithText(GOOD_MOOD_TEXT)
-//        defaultMoodText.assertIsDisplayed()
+
+        val defaultMoodText = composeTestRule.onNodeWithText(GOOD_MOOD_TEST_TAG)
+        defaultMoodText.assertIsDisplayed()
 //        composeTestRule.onNodeWithText(BAD_MOOD_TEXT).assertIsNotDisplayed()
 
         composeTestRule.onNodeWithText(SUBMIT_TEXT).assertIsDisplayed()
@@ -31,8 +31,8 @@ class MainActivityTest {
     companion object {
         private const val HOW_ARE_YOU_TEXT = "How are you feeling today?"
         private const val SWIPE_HELP_TEXT = "(Swipe to change mood)"
-//        private const val GOOD_MOOD_TEXT = "Doing Great!"
-//        private const val BAD_MOOD_TEXT = "Not So Great"
+    private const val GOOD_MOOD_TEST_TAG = "mood0-text"
+//    private const val BAD_MOOD_TEST_TAG = "mood1-text"
         private const val SUBMIT_TEXT = "Submit"
         private const val DISCLAIMER_TEXT = "This is completely anonymous."
     }
