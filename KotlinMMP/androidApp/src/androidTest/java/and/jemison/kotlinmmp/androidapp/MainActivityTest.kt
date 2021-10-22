6cc4025529cc63e4d000d52e29b3f2c45dab860a
Jack Rimmer <jack.rimmer@and.digital>
2021-10-22 12:41:32 +0100

    Tag update

--- KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
+++ KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MainActivityTest.kt
@@ -20,20 +20,20 @@ class MainActivityTest {
         composeTestRule.onNodeWithText(HOW_ARE_YOU_TEXT).assertIsDisplayed()
         composeTestRule.onNodeWithText(SWIPE_HELP_TEXT).assertIsDisplayed()
 
-//        val defaultMoodText = composeTestRule.onNodeWithTag(GOOD_MOOD_TEST_TAG)
-//        defaultMoodText.assertIsDisplayed()
-//        composeTestRule.onNodeWithTag(BAD_MOOD_TEXT).assertIsNotDisplayed()
+        val defaultMoodText = composeTestRule.onNodeWithTag(GOOD_MOOD_TEST_TAG)
+        defaultMoodText.assertIsDisplayed()
+//        composeTestRule.onNodeWithTag(BAD_MOOD_TEST_TAG).assertIsNotDisplayed()
 
-        composeTestRule.onNodeWithText(SUBMIT_TEXT).assertIsDisplayed()
-        composeTestRule.onNodeWithText(DISCLAIMER_TEXT).assertIsDisplayed()
+//        composeTestRule.onNodeWithText(SUBMIT_TEXT).assertIsDisplayed()
+//        composeTestRule.onNodeWithText(DISCLAIMER_TEXT).assertIsDisplayed()
     }
 //
     companion object {
         private const val HOW_ARE_YOU_TEXT = "How are you feeling today?"
         private const val SWIPE_HELP_TEXT = "(Swipe to change mood)"
-//      private const val GOOD_MOOD_TEST_TAG = "mood0-text"
-//      private const val BAD_MOOD_TEST_TAG = "mood1-text"
-        private const val SUBMIT_TEXT = "Submit"
-        private const val DISCLAIMER_TEXT = "This is completely anonymous."
+        private const val GOOD_MOOD_TEST_TAG = "mood0-text"
+//        private const val BAD_MOOD_TEST_TAG = "mood1-text"
+//        private const val SUBMIT_TEXT = "Submit"
+//        private const val DISCLAIMER_TEXT = "This is completely anonymous."
     }
 }
