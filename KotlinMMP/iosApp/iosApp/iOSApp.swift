4d4b7f131fc00d7f3fd135b3b13ea5f83687e2b0
MariliaE <marilia.elia@and.digital>
2021-10-29 10:58:37 +0100

    removed Amplify Plugin to test

--- KotlinMMP/iosApp/iosApp/iOSApp.swift
+++ KotlinMMP/iosApp/iosApp/iOSApp.swift
@@ -9,7 +9,7 @@ struct iOSApp: App {
         AppCenter.start(withAppSecret: "388e8513-eaaf-4c7d-8bca-dfcddcc20128", services: [Analytics.self, Crashes.self])
     }
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    // @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
 	var body: some Scene {
 		WindowGroup {
