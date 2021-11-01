d9ec7a2a1884021d5875019add5b9435f3ab7bad
Cesim Civisilli <cesim.civisilli@and.digital>
2021-11-01 13:13:23 +0200

    change mood to moodInfo and reinstall pods

---
+++ KotlinMMP/androidApp/src/androidTest/java/and/jemison/kotlinmmp/androidapp/MoodInfoPagerTest.kt
@@ -0,0 +1,65 @@
+package and.jemison.kotlinmmp.androidapp
+
+import and.jemison.kotlinmmp.androidapp.components.mood.MoodPager
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.google.accompanist.pager.ExperimentalPagerApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MoodInfoPagerTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @ExperimentalPagerApi
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            MoodPager()
+        }
+    }
+
+    @Test
+    fun defaultMoodRenders() {
+        composeTestRule.onNodeWithTag(GOOD_MOOD_TEST_TAG).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(BAD_MOOD_TEST_TAG).assertIsNotDisplayed()
+    }
+
+    @Test
+    fun swipeTextBetweenMoods() {
+        val goodMoodText = composeTestRule.onNodeWithTag(GOOD_MOOD_TEST_TAG)
+        val goodMoodImage =  composeTestRule.onNodeWithTag(ROCKET_UP_TEST_TAG)
+
+        val badMoodText = composeTestRule.onNodeWithTag(BAD_MOOD_TEST_TAG)
+        val badMoodImage = composeTestRule.onNodeWithTag(ROCKET_DOWN_TEST_TAG)
+
+        goodMoodText.performGesture { swipeLeft() }
+
+        badMoodText.assertIsDisplayed()
+        badMoodImage.assertIsDisplayed()
+        goodMoodText.assertIsNotDisplayed()
+        goodMoodImage.assertIsNotDisplayed()
+
+        badMoodText.performGesture { swipeRight() }
+
+        goodMoodText.assertIsDisplayed()
+        goodMoodImage.assertIsDisplayed()
+        badMoodText.assertIsNotDisplayed()
+        badMoodImage.assertIsNotDisplayed()
+    }
+
+    companion object {
+        private const val GOOD_MOOD_TEST_TAG = "mood0-text"
+        private const val BAD_MOOD_TEST_TAG = "mood1-text"
+
+        private const val ROCKET_UP_TEST_TAG = "mood0-image"
+        private const val ROCKET_DOWN_TEST_TAG = "mood1-image"
+    }
+}
---
+++ KotlinMMP/shared/src/commonMain/kotlin/and/jemison/kotlinmmp/shared/MoodInfo.kt
@@ -0,0 +1,8 @@
+package and.jemison.kotlinmmp.shared
+
+data class MoodInfo(
+    val text: String,
+    val textColour: String,
+    val image: String,
+    val contentDescription: String,
+) {}
