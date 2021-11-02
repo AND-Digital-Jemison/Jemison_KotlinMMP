39cc0867bf2deb97fe2d3ef586b998e80fa778a2
Cesim Civisilli <cesim.civisilli@and.digital>
2021-11-02 14:22:35 +0200

    remove aws and amplify configuration json from gitignore and push them to iosApp

--- KotlinMMP/iosApp/amplify/generated/models/AmplifyModels.swift
+++ KotlinMMP/iosApp/amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "05216a74bfbe320620be35a2560499cd"
+  public let version: String = "d0406fa63da075a4e653cc27952c2717"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: Mood.self)
--- KotlinMMP/iosApp/amplify/generated/models/Mood+Schema.swift
+++ KotlinMMP/iosApp/amplify/generated/models/Mood+Schema.swift
@@ -7,8 +7,6 @@ extension Mood {
    public enum CodingKeys: String, ModelKey {
     case id
     case text
-    case createdAt
-    case updatedAt
   }
   
   public static let keys = CodingKeys.self
@@ -17,17 +15,11 @@ extension Mood {
   public static let schema = defineSchema { model in
     let mood = Mood.keys
     
-    model.authRules = [
-      rule(allow: .public, operations: [.create, .update, .delete, .read])
-    ]
-    
     model.pluralName = "Moods"
     
     model.fields(
       .id(),
-      .field(mood.text, is: .optional, ofType: .string),
-      .field(mood.createdAt, is: .optional, isReadOnly: true, ofType: .dateTime),
-      .field(mood.updatedAt, is: .optional, isReadOnly: true, ofType: .dateTime)
+      .field(mood.text, is: .optional, ofType: .string)
     )
     }
 }
