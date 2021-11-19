19816cd519bfc16e80b11f05f1a669d32d1d57c6
MariliaE <marilia.elia@and.digital>
2021-11-19 16:18:07 +0000

    env name added in iOS

--- KotlinMMP/iosApp/amplify-init-ios.sh
+++ KotlinMMP/iosApp/amplify-init-ios.sh
@@ -1,9 +1,12 @@
+source "../gradle.properties"
+echo "Env name value: $ENV_NAME"
+
 FRONTEND="{\
 \"frontend\":\"ios\"\
 }"
 
 AMPLIFY="{\
-\"envName\":\"test\",\
+\"envName\":\"$ENV_NAME\",\
 \"appId\":\"d194x8oiwokw1k\",\
 \"defaultEditor\":\"AndroidStudio\"\
 }"
