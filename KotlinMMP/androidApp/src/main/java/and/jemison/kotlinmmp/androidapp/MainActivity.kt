eb4acf525417dc271350826f3a6ae4bc18cfa981
Cesim Civisilli <cesim.civisilli@and.digital>
2021-11-24 10:51:35 +0000

    remove dotenv files

--- KotlinMMP/androidApp/src/main/java/and/jemison/kotlinmmp/androidapp/MainActivity.kt
+++ KotlinMMP/androidApp/src/main/java/and/jemison/kotlinmmp/androidapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package and.jemison.kotlinmmp.androidapp
 
-import io.github.cdimascio.dotenv.dotenv
 import and.jemison.kotlinmmp.androidapp.backend.AmplifyQueries
 import and.jemison.kotlinmmp.androidapp.components.mood.MoodHeader
 import and.jemison.kotlinmmp.androidapp.components.mood.MoodPager
@@ -8,7 +7,6 @@ import and.jemison.kotlinmmp.androidapp.components.mood.MoodScreen
 import and.jemison.kotlinmmp.androidapp.components.mood.MoodSubmit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -21,11 +19,6 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
-
-val dotenv = dotenv {
-    directory = "./assets"
-    filename = "env"
-}
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
         val amplify = AmplifyQueries()
         amplify.configureAmplify(applicationContext)
-        Log.i("ENV_NAME", dotenv["ENV_NAME"])
         setContent {
             Image(
                 painter = painterResource(R.drawable.purple_background),
